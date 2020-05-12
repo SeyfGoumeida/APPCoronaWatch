87ec87db1ada132c794df8b8da48f80b405b8cf8
nedjme <farah.nedjmeddine@gmail.com>
2020-05-12 18:56:08 +0100

    Commit  , profile

--- app/src/main/java/com/example/coronawatch/ui/profil/ProfilFragment.kt
+++ app/src/main/java/com/example/coronawatch/ui/profil/ProfilFragment.kt
@@ -1,6 +1,7 @@
 package com.example.coronawatch.ui.profil
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,21 +9,41 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.coronawatch.DataClases.User
 import com.example.coronawatch.R
+import com.example.coronawatch.Retrofit.IAPI
+import com.example.coronawatch.Retrofit.RetrofitClient
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_profil.*
 
 class ProfilFragment : Fragment() {
 
-    private lateinit var profilViewModel: ProfilViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        profilViewModel =
-                ViewModelProviders.of(this).get(ProfilViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_profil, container, false)
 
-        return root
+        return inflater.inflate(R.layout.fragment_profil, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val gson = Gson()
+        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+        val jsonString = preferences.getString("user","{}")
+        val  user : User  = gson.fromJson(jsonString , User::class.java)
+
+        profile_email.text = user.email
+        profile_username.text = user.username
+
+
+    }
+
+
 }
