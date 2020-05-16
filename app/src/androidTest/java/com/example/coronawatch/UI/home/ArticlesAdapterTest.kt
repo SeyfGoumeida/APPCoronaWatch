39ab5f4a6de22c8ead3c2205bcba390745802203
nedjme <farah.nedjmeddine@gmail.com>
2020-05-16 04:18:11 +0100

    Commit  , full test

--- app/src/androidTest/java/com/example/coronawatch/UI/home/ArticlesAdapterTest.kt
+++ app/src/androidTest/java/com/example/coronawatch/UI/home/ArticlesAdapterTest.kt
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
@@ -26,23 +27,52 @@ class ArticlesAdapterTest {
     @JvmField
     val rule :ActivityTestRule<MainActivity> =ActivityTestRule(MainActivity::class.java)
 
-    @Before
-    fun signin() {
+    @Test
+   fun CommentingProccess () {
 
-        onView(withId(R.id.user_btn)).perform(click())
-        onView(withId(R.id.viewPager)).perform(swipeLeft())
-        Thread.sleep(1000)
-        onView(withId(R.id.username_login)).perform(typeText("tester@esi.dz"))
-        onView(withId(R.id.password_login)).perform(typeText("test"))
-        onView(withId(R.id.loginbtn)).perform(click())
-        Thread.sleep(3000)
+        val random = (0..100).random()
+        val username = "test$random"
+        val email = "test$random@esi.dz"
+        val password = "test"
 
+        SignUp(username , email  , password)
+        LogOut()
+        SignIn(email , password)
+        UserCanComment("trying the whole thing yay")
+        LogOut()
 
 
     }
 
-    @Test
-    fun UserCanComment() {
+
+    fun SignUp(username: String , email:String , password: String) {
+
+        onView(withId(R.id.user_btn)).perform(click())
+        onView(withId(R.id.username)).perform(typeText(username), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.email)).perform(typeText(email), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.password)).perform(typeText(password),closeSoftKeyboard())
+        onView(withId(R.id.password2)).perform(typeText(password),closeSoftKeyboard())
+        onView(withId(R.id.signupbtn)).perform(click())
+        Thread.sleep(3000)
+
+        return
+    }
+
+
+    fun SignIn( username : String , password : String) {
+
+        onView(withId(R.id.user_btn)).perform(click())
+        onView(withId(R.id.viewPager)).perform(swipeLeft())
+        Thread.sleep(1000)
+        onView(withId(R.id.username_login)).perform(typeText(username))
+        onView(withId(R.id.password_login)).perform(typeText(password))
+        onView(withId(R.id.loginbtn)).perform(click())
+        Thread.sleep(3000)
+
+    }
+
+
+    fun UserCanComment( comment : String) {
 
         fun type(viewId: Int , comment :String) = object : ViewAction {
             override fun getConstraints() = null
@@ -70,7 +100,7 @@ class ArticlesAdapterTest {
             .perform(
                 RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                     0,
-                    type(R.id.comment_content, "testing again expresso")
+                    type(R.id.comment_content, comment)
                 )
             )
 
@@ -93,8 +123,7 @@ class ArticlesAdapterTest {
     }
 
 
-        @After
-        fun logout() {
+        fun LogOut() {
 
         onView(withId(R.id.navigation_profil)).perform(click())
             Thread.sleep(4000)
